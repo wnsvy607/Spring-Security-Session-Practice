25ca7775db61becad7b2c4a7bf8e8c53622e8013
풀그/이준표 <85255237+wnsvy607@users.noreply.github.com>
2023-06-15 03:04:07 +0900

    [FEAT] Redis Session 저장소 변경

--- src/main/java/com/jp/session/controller/TestController.java
+++ src/main/java/com/jp/session/controller/TestController.java
@@ -18,9 +18,6 @@ public class TestController {
 	@GetMapping("/test")
 	public String hello(HttpSession session) {
 		if (session != null) {
-			if(session.getAttribute("name") != null)
-				return (String) session.getAttribute("name");
-			session.setAttribute("name", "hello");
 			return session.getId();
 		}
 		return "new one";
